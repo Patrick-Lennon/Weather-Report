026779d715b3044beae08cd1cecdd7c7e6b5d7ab
Patrick Lennon <patlennonri@gmail.com>
2018-05-17 11:17:36 -0400

    basic page with bootstrap loaded in

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
-<!DOCTYPE html>
+
 <html land="en">
     <head>
-        <meta charset="utf-8">
+        <meta charset="UTF-8">
         <title>Forecast</title>
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
     </head>
